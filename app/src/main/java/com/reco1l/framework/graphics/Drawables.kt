45a08186ca72a2af3da3add8cbb965eedbef779e
Reco1l <geronimoferruccio15@gmail.com>
2023-10-14 17:42:58 +0200

    [Framework] Remove RoundShape and use GradientDrawable as a better alternative

--- app/src/main/java/com/reco1l/framework/graphics/Drawables.kt
+++ app/src/main/java/com/reco1l/framework/graphics/Drawables.kt
@@ -2,8 +2,10 @@ package com.reco1l.framework.graphics
 
 import android.graphics.drawable.ClipDrawable
 import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.LayerDrawable
 import android.view.Gravity
+import com.reco1l.framework.lang.orCatch
 
 
 // ClipDrawable
@@ -34,9 +36,34 @@ fun LayerDrawable.forEach(block: (Drawable) -> Unit)
 fun LayerDrawable(vararg layers: Drawable) = LayerDrawable(layers)
 
 
-// Size
+// GradientDrawable
 
-fun Drawable.setSize(width: Int = bounds.width(), height: Int = bounds.height())
+/**
+ * Apply a radius to the specified anchor or all anchors.
+ *
+ * @param anchor The anchor to apply the radius, if `null` is passed the radius will be applied
+ * to all anchors.
+ */
+fun GradientDrawable.setRadius(radius: Float, @CornerAnchor anchor: Int? = null)
 {
-    setBounds(0, 0, width, height)
+    // orCatch -> Apparently in API 24 the method "getCornerRadii()" causes an NPE by itself if there
+    // wasn't a corner radii previously set, this is fixed in newer APIs but for older we've to handle
+    // the bug thanks Android.
+    val radii = { cornerRadii }.orCatch { null } ?: FloatArray(8)
+
+    if (anchor == null)
+    {
+        radii.fill(radius)
+        cornerRadii = radii
+        return
+    }
+
+    when (anchor)
+    {
+        Anchor.TOP_LEFT -> radii.fill(radius, 0, 2)
+        Anchor.TOP_RIGHT -> radii.fill(radius, 2, 4)
+        Anchor.BOTTOM_LEFT -> radii.fill(radius, 4, 6)
+        Anchor.BOTTOM_RIGHT -> radii.fill(radius, 6, 8)
+    }
+    cornerRadii = radii
 }
