f95a2e89dcd8198eb6c0ff9fd6836773098cc78b
Reco1l <geronimoferruccio15@gmail.com>
2023-10-15 21:59:14 +0200

    [Framework] Add lists views extensions for Android module

---
+++ app/src/main/java/com/reco1l/framework/android/views/Lists.kt
@@ -0,0 +1,16 @@
+package com.reco1l.framework.android.views
+
+import android.widget.LinearLayout.VERTICAL
+import androidx.recyclerview.widget.LinearLayoutManager
+import game.rimu.ui.views.RecyclerView
+
+
+var RecyclerView.orientation
+    get() = (layoutManager as? LinearLayoutManager)?.orientation
+    set(value)
+    {
+        if (layoutManager !is LinearLayoutManager)
+            layoutManager = LinearLayoutManager(context)
+
+        (layoutManager as? LinearLayoutManager)?.orientation = value ?: VERTICAL
+    }
