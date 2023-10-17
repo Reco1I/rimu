f400a21d54fbd0cfc12311d6fcd2c035302e19b0
Reco1l <geronimoferruccio15@gmail.com>
2023-10-17 16:32:17 -0300

    Support seeking in the MusicPlayerBox

--- app/src/main/java/game/rimu/ui/layouts/MusicPlayerBox.kt
+++ app/src/main/java/game/rimu/ui/layouts/MusicPlayerBox.kt
@@ -3,14 +3,17 @@ package game.rimu.ui.layouts
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.view.animation.BounceInterpolator
 import android.view.animation.DecelerateInterpolator
+import android.widget.SeekBar.OnSeekBarChangeListener
 import com.reco1l.basskt.AudioState
 import com.reco1l.framework.android.views.animate
+import com.reco1l.framework.android.views.localized
 import com.reco1l.framework.android.views.scale
 import com.reco1l.framework.android.views.setConstraints
 import com.reco1l.framework.android.views.setListeners
 import com.reco1l.framework.android.views.setScale
 import com.reco1l.framework.graphics.Anchor
 import com.reco1l.framework.lang.dateFormatFor
+import game.rimu.R
 import game.rimu.android.RimuContext
 import game.rimu.management.beatmap.IBeatmapObserver
 import game.rimu.management.beatmap.WorkingBeatmap
@@ -20,9 +23,11 @@ import game.rimu.ui.views.IconButton
 import game.rimu.ui.views.SeekBar
 import game.rimu.ui.views.TextView
 import game.rimu.ui.views.addons.setTouchHandler
+import game.rimu.ui.views.setTextAnimated
 import org.andengine.engine.handler.IUpdateHandler
 import java.text.SimpleDateFormat
 import kotlin.reflect.KClass
+import android.widget.SeekBar as AndroidSeekBar
 
 class MusicPlayerBox(ctx: RimuContext) :
     RimuLayout(ctx),
@@ -33,65 +38,90 @@ class MusicPlayerBox(ctx: RimuContext) :
     override var layer: KClass<out LayoutLayer> = LayerOverlay::class
 
 
-    val titleText = TextView {
+    private var dateFormat = dateFormatFor(1L)
 
-        text = "Unknown"
+    // Determines if the seekbar is being change by the user.
+    private var isSeeking = false
+
+
+    private val titleText = TextView {
+
+        text = localized(R.string.metadata_unknown)
 
     }
 
-    val artistText = TextView {
+    private val artistText = TextView {
 
-        text = "Unknown"
+        dimensions.fontSize = 10
+        rules.fontColorFactor = 0.75f
+
+        text = localized(R.string.metadata_unknown)
 
         setConstraints(
             target = titleText,
             topToTarget = Anchor.BOTTOM
         )
-
-        dimensions.fontSize = 10
-        alpha = 0.8f
     }
 
-    val timeText = TextView {
+    private val timeText = TextView {
 
-        text = "00:00"
         rules.fontColorFactor = 0.8f
-
-        setConstraints(
-            target = artistText,
-            topToTarget = Anchor.BOTTOM
-        )
+        text = dateFormat.format(0L)
 
         dimensions.apply {
             fontSize = 8
             marginTop = 12
         }
 
+        setConstraints(
+            target = artistText,
+            topToTarget = Anchor.BOTTOM
+        )
     }
 
-    val lengthText = TextView {
+    private val lengthText = TextView {
 
-        text = "00:00"
         dimensions.fontSize = 8
         rules.fontColorFactor = 0.8f
+
+        text = dateFormat.format(0L)
 
         setConstraints(target = timeText, topToTarget = Anchor.TOP)
         setConstraints(rightToTarget = Anchor.RIGHT)
     }
 
 
-    val seekBar = SeekBar {
+    private val seekBar = SeekBar {
+
+        dimensions.marginTop = 8
 
         setConstraints(
             target = timeText,
             topToTarget = Anchor.BOTTOM
         )
 
-        dimensions.marginTop = 8
+        setOnSeekBarChangeListener(object : OnSeekBarChangeListener
+        {
+            override fun onProgressChanged(
+                seekBar: AndroidSeekBar,
+                progress: Int,
+                fromUser: Boolean
+            ) = Unit
+
+            override fun onStartTrackingTouch(seekBar: AndroidSeekBar)
+            {
+                isSeeking = true
+            }
+
+            override fun onStopTrackingTouch(seekBar: AndroidSeekBar)
+            {
+                ctx.beatmaps.current?.stream?.position = seekBar.progress.toLong()
+                isSeeking = false
+            }
+        })
     }
 
-
-    val playButton = IconButton {
+    private val playButton = IconButton {
 
         rules.image = "icon-pause"
 
@@ -133,54 +163,52 @@ class MusicPlayerBox(ctx: RimuContext) :
         }
     }
 
-    val previousButton = IconButton {
-
-        rules.image = "icon-previous"
-
-        setConstraints(
-            target = playButton,
-            topToTarget = Anchor.TOP,
-            bottomToTarget = Anchor.BOTTOM,
-            rightToTarget = Anchor.LEFT
-        )
-
-        dimensions.apply {
-            marginRight = 8
-            size(50)
-        }
-
-        setTouchHandler {
-            onActionUp = { ctx.beatmaps.previous() }
-        }
-    }
-
-    val nextButton = IconButton {
-
-        rules.image = "icon-next"
-
-        setConstraints(
-            target = playButton,
-            topToTarget = Anchor.TOP,
-            leftToTarget = Anchor.RIGHT,
-            bottomToTarget = Anchor.BOTTOM
-        )
-
-        dimensions.apply {
-            marginLeft = 8
-            size(50)
-        }
-
-        setTouchHandler {
-            onActionUp = { ctx.beatmaps.next() }
-        }
-    }
-
-
-    private lateinit var dateFormat: SimpleDateFormat
-
 
     init
     {
+
+        IconButton {
+
+            rules.image = "icon-previous"
+
+            setConstraints(
+                target = playButton,
+                topToTarget = Anchor.TOP,
+                bottomToTarget = Anchor.BOTTOM,
+                rightToTarget = Anchor.LEFT
+            )
+
+            dimensions.apply {
+                marginRight = 8
+                size(50)
+            }
+
+            setTouchHandler {
+                onActionUp = { ctx.beatmaps.previous() }
+            }
+        }
+
+        IconButton {
+
+            rules.image = "icon-next"
+
+            setConstraints(
+                target = playButton,
+                topToTarget = Anchor.TOP,
+                leftToTarget = Anchor.RIGHT,
+                bottomToTarget = Anchor.BOTTOM
+            )
+
+            dimensions.apply {
+                marginLeft = 8
+                size(50)
+            }
+
+            setTouchHandler {
+                onActionUp = { ctx.beatmaps.next() }
+            }
+        }
+
         dimensions.apply {
             width = 200
             height = WRAP_CONTENT
@@ -201,27 +229,47 @@ class MusicPlayerBox(ctx: RimuContext) :
 
     override fun onMusicChange(beatmap: WorkingBeatmap?)
     {
+        val length = beatmap?.stream?.length ?: 1L
+
+        // Creating date format once instead of creating a new one everytime we want to format (consider
+        // timestamps texts that indeed are formatted every tick in update thread)
+        dateFormat = dateFormatFor(length)
+
         mainThread {
 
-            titleText.text = beatmap?.source?.title ?: "Unknown"
-            artistText.text = beatmap?.source?.let { "${it.artist}\n${it.version}" } ?: "Unknown"
+            titleText.setTextAnimated(beatmap?.source?.title
+                ?: localized(R.string.metadata_unknown))
 
-            val length = beatmap?.stream?.length ?: 1L
-
-            dateFormat = dateFormatFor(length)
+            artistText.setTextAnimated(beatmap?.source?.let { "${it.artist}\n${it.version}" }
+                ?: localized(R.string.metadata_unknown))
 
             lengthText.text = dateFormat.format(length)
+
+            // Seekbar max value will equal to the length of the song so we can use absolute
+            // positioning when seeking.
             seekBar.max = length.toInt()
         }
     }
 
     override fun onUpdate(secElapsed: Float)
     {
-        mainThread {
-            val position = ctx.beatmaps.current?.stream?.position ?: 0L
+        val position = when
+        {
+            // If seeking the position is determined by the thumb position.
+            isSeeking -> seekBar.progress
 
+            // If not then it's determined by the audio stream position.
+            else -> ctx.beatmaps.current?.stream?.position ?: 0L
+        }
+
+        mainThread {
+
+            // Timestamp text will always update to either thumb position or audios stream position.
             timeText.text = dateFormat.format(position)
-            seekBar.progress = position.toInt()
+
+            // If seeking we should avoid update twice the seekbar progress.
+            if (!isSeeking)
+                seekBar.progress = position.toInt()
         }
     }
 
@@ -235,6 +283,8 @@ class MusicPlayerBox(ctx: RimuContext) :
 
         super.onAttachedToWindow()
 
+        // Bounce animation
+
         alpha = 0f
         setScale(0.8f)
 
@@ -246,24 +296,26 @@ class MusicPlayerBox(ctx: RimuContext) :
             interpolator = BounceInterpolator()
         }
 
+        // Updating information just in case.
         onMusicChange(ctx.beatmaps.current)
+
         ctx.engine.registerUpdateHandler(this)
     }
 
     override fun onDetachedFromWindow()
     {
         super.onDetachedFromWindow()
+
         ctx.engine.unregisterUpdateHandler(this)
     }
 
     override fun hide()
     {
         animate {
-
-            scale(0.8f)
+            scale(0.9f)
             alpha(0f)
 
-            duration = 200
+            duration = 150
             interpolator = DecelerateInterpolator()
 
             setListeners(onEnd = { super.hide() })
