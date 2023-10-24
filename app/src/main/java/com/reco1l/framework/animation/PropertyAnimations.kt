247551eabde1ce3d19fb5acf8cf623e59dc01030
Reco1l <geronimoferruccio15@gmail.com>
2023-10-24 02:54:16 +0200

    [Framework] Add function animations

--- app/src/main/java/com/reco1l/framework/animation/PropertyAnimations.kt
+++ app/src/main/java/com/reco1l/framework/animation/PropertyAnimations.kt
@@ -2,24 +2,56 @@ package com.reco1l.framework.animation
 
 import android.animation.TimeInterpolator
 import android.animation.ValueAnimator
-import android.animation.ValueAnimator.ofFloat
-import android.animation.ValueAnimator.ofInt
+import kotlin.reflect.KFunction1
 import kotlin.reflect.KMutableProperty0
 
 
-fun KMutableProperty0<Float>.animateTo(
-    target: Float,
+// Floats
+
+private fun animateFloat(
+    onApply: (Float) -> Unit,
+    from: Float,
+    to: Float,
     end: Long,
     delay: Long = 0,
-    ease: TimeInterpolator? = null
+    ease: TimeInterpolator?
 
-): ValueAnimator = ofFloat(get(), target).apply {
+) = ValueAnimator.ofFloat(from, to).apply {
 
     duration = end
     startDelay = delay
     interpolator = ease ?: Ease.LINEAR
 
-    addUpdateListener { set(it.animatedValue as Float) }
+    addUpdateListener { onApply(it.animatedValue as Float) }
+    start()
+}
+
+fun KMutableProperty0<Float>.animateTo(
+    to: Float,
+    end: Long,
+    delay: Long = 0,
+    ease: TimeInterpolator? = null
+
+): ValueAnimator = animateFloat(this::set, get(), to, end, delay, ease)
+
+
+// Integers
+
+private fun animateInt(
+    onApply: (Int) -> Unit,
+    from: Int,
+    to: Int,
+    end: Long,
+    delay: Long = 0,
+    ease: TimeInterpolator?
+
+) = ValueAnimator.ofInt(from, to).apply {
+
+    duration = end
+    startDelay = delay
+    interpolator = ease ?: Ease.LINEAR
+
+    addUpdateListener { onApply(it.animatedValue as Int) }
     start()
 }
 
@@ -29,12 +61,25 @@ fun KMutableProperty0<Int>.animateTo(
     delay: Long = 0,
     ease: TimeInterpolator? = null
 
-): ValueAnimator = ofInt(get(), target).apply {
+): ValueAnimator = animateInt(this::set, get(), target, end, delay, ease)
 
-    duration = end
-    startDelay = delay
-    interpolator = ease ?: Ease.LINEAR
 
-    addUpdateListener { set(it.animatedValue as Int) }
-    start()
-}
+// Functions
+
+fun KFunction1<Int, Any>.animate(
+    from: Int = 0,
+    to: Int,
+    end: Long,
+    delay: Long = 0,
+    ease: TimeInterpolator? = null
+
+) = animateInt(this::invoke, from, to, end, delay, ease)
+
+fun KFunction1<Float, Any>.animate(
+    from: Float = 0f,
+    to: Float,
+    end: Long,
+    delay: Long = 0,
+    ease: TimeInterpolator? = null
+
+) = animateFloat(this::invoke, from, to, end, delay, ease)
