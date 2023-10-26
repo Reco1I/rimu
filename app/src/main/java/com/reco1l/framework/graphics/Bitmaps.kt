58b047b14baf2b6db57a1e346fd2efb4af832383
Reco1l <geronimoferruccio15@gmail.com>
2023-10-27 00:09:57 +0200

    [Framework] Add WrappingTexture support feature for AndEngine

--- app/src/main/java/com/reco1l/framework/graphics/Bitmaps.kt
+++ app/src/main/java/com/reco1l/framework/graphics/Bitmaps.kt
@@ -1,12 +1,18 @@
 package com.reco1l.framework.graphics
 
 import android.graphics.Bitmap
+import android.graphics.Bitmap.Config
 import android.graphics.Bitmap.Config.ARGB_8888
 import android.graphics.BitmapFactory
 import android.graphics.BitmapFactory.Options
 import androidx.core.graphics.applyCanvas
 import com.caverock.androidsvg.SVG
 import com.reco1l.framework.kotlin.orCatch
+import com.reco1l.framework.support.WrappingTexture
+import org.andengine.opengl.texture.ITextureStateListener
+import org.andengine.opengl.texture.PixelFormat
+import org.andengine.opengl.texture.TextureManager
+import org.andengine.opengl.texture.TextureOptions
 import java.io.InputStream
 import kotlin.math.roundToInt
 
@@ -35,4 +41,30 @@ fun InputStream.toBitmap(): Bitmap? = use {
         null,
         Options().apply { inPreferredConfig = ARGB_8888 }
     )
+}
+
+/**
+ * Creates a [WrappingTexture] instance using this bitmap as source.
+ */
+fun Bitmap.toTexture(
+    manager: TextureManager,
+    options: TextureOptions = TextureOptions.DEFAULT,
+    listener: ITextureStateListener? = null
+
+) = WrappingTexture(this, manager, options, listener)
+
+/**
+ * Get the [PixelFormat] equivalent of [Bitmap.Config].
+ */
+fun Bitmap.getPixelFormat(): PixelFormat
+{
+    return when (config)
+    {
+        Config.ALPHA_8 -> PixelFormat.A_8
+        Config.RGB_565 -> PixelFormat.RGB_565
+        Config.ARGB_4444 -> PixelFormat.RGBA_4444
+        Config.ARGB_8888 -> PixelFormat.RGBA_8888
+
+        else -> PixelFormat.UNDEFINED
+    }
 }
---
+++ app/src/main/java/com/reco1l/framework/support/Textures.kt
@@ -0,0 +1,77 @@
+package com.reco1l.framework.support
+
+import android.graphics.Bitmap
+import android.opengl.GLES20
+import android.opengl.GLES20.GL_TEXTURE_2D
+import android.opengl.GLES20.GL_UNPACK_ALIGNMENT
+import android.opengl.GLUtils
+import com.reco1l.framework.graphics.getPixelFormat
+import com.reco1l.framework.isPowerOfTwo
+import org.andengine.opengl.texture.ITextureStateListener
+import org.andengine.opengl.texture.PixelFormat.RGBA_8888
+import org.andengine.opengl.texture.Texture
+import org.andengine.opengl.texture.bitmap.BitmapTexture
+import org.andengine.opengl.texture.TextureManager
+import org.andengine.opengl.texture.TextureOptions
+import org.andengine.opengl.texture.region.TextureRegion
+import org.andengine.opengl.util.GLState
+import org.andengine.opengl.util.GLState.GL_UNPACK_ALIGNMENT_DEFAULT
+
+
+/**
+ * Improved version of [BitmapTexture] from AndEngine which allows to reuse an already created bitmap
+ * without recycling it (because it's not needed anymore).
+ */
+class WrappingTexture(
+
+    val bitmap: Bitmap,
+
+    manager: TextureManager,
+
+    options: TextureOptions = TextureOptions.DEFAULT,
+
+    listener: ITextureStateListener? = null
+
+) : Texture(manager, bitmap.getPixelFormat(), options, listener)
+{
+
+    /**
+     * The shared instance of the region holding this texture.
+     * Transformations shouldn't be applied to this instance, use [toTextureRegion] instead.
+     */
+    val sharedTextureRegion by lazy { toTextureRegion() }
+
+
+    private val useDefaultAlignment = pixelFormat == RGBA_8888
+            && width.isPowerOfTwo()
+            && height.isPowerOfTwo()
+
+
+    override fun getWidth() = bitmap.width
+
+    override fun getHeight() = bitmap.height
+
+
+    override fun writeTextureToHardware(gl: GLState)
+    {
+        if (!useDefaultAlignment)
+            GLES20.glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+
+        when (mTextureOptions.mPreMultiplyAlpha)
+        {
+            true -> GLUtils.texImage2D(GL_TEXTURE_2D, 0, bitmap, 0)
+
+            false -> gl.glTexImage2D(GL_TEXTURE_2D, 0, bitmap, 0, mPixelFormat)
+        }
+
+        if (!useDefaultAlignment)
+            GLES20.glPixelStorei(GL_UNPACK_ALIGNMENT, GL_UNPACK_ALIGNMENT_DEFAULT)
+    }
+
+
+}
+
+/**
+ * Returns a new [TextureRegion] with this texture as source.
+ */
+fun WrappingTexture.toTextureRegion() = TextureRegion(this, 0f, 0f, width.toFloat(), height.toFloat())
